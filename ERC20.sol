b9fe49a6eca45fad7441d01524813a68a101e93f
rajdeepsingh006 <147084619+rajdeepsingh006@users.noreply.github.com>
2024-03-24 16:18:07 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+
+contract RAJ is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner) ERC20("RAJ", "RJ") Ownable(initialOwner) {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
