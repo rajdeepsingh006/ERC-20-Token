6f2984b245df4c962abf15b536ccba9b793c26ff
rajdeepsingh006 <147084619+rajdeepsingh006@users.noreply.github.com>
2024-03-25 15:29:23 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -8,10 +8,19 @@ import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 
 contract RAJ is ERC20, ERC20Burnable, Ownable {
     constructor(address initialOwner) ERC20("RAJ", "RJ") Ownable(initialOwner) {
-        _mint(msg.sender, 100 * 10 ** decimals());
+        _mint(msg.sender, 1 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+        function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) override public virtual {
+        _burn(_msgSender(), amount);
     }
 }
